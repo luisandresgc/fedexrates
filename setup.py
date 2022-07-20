6e08d939de30313bb770337043963fa0277b5683
Andres Garcia <desarrollo02@cebsa.mx>
2022-07-20 17:05:45 -0500

    informacion de cuenta

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ readme = open("./README.md", "r")
 
 
 setup(
-    name='nelsonsaludo',
-    packages=['nelsonsaludo'],  # this must be the same as the name above
+    name='fedexrates',
+    packages=['fedexrates'],  # this must be the same as the name above
     version='0.1',
-    description='Esta es la descripcion de mi paquete',
+    description='Libreria para conseguir cotizaciones sobre envio de paquetes con la API de shipengine (FedEx)',
     long_description=readme.read(),
     long_description_content_type='text/markdown',
     author='Luis Contreras',
-    author_email='',
+    author_email='desarrollo02@cebsa.mx',
     # use the URL to the github repo
-    url='https://github.com/nelsonher019/nelsonsaludo',
-    download_url='https://github.com/nelsonher019/nelsonsaludo/tarball/0.1',
+    url='https://github.com/luisandresgc/fedexrates',
+    download_url='https://github.com/luisandresgc/fedexrates/tarball/0.1',
     keywords=['testing', 'logging', 'example'],
     classifiers=[ ],
     license='MIT',
