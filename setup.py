c340771392c2e6d7674ed42d8a13630edb077fc0
Andres Garcia <desarrollo02@cebsa.mx>
2022-07-20 18:54:38 -0500

    version 0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='fedexrates',
     install_requires=install_requires,
     packages=['fedexrates', ],  # this must be the same as the name above
-    version='0.2',
+    version='0.3',
     description='Libreria para conseguir cotizaciones sobre envio de paquetes con la API de shipengine (FedEx)',
     long_description=readme.read(),
     long_description_content_type='text/markdown',
@@ -22,7 +22,7 @@ setup(
     author_email='desarrollo02@cebsa.mx',
     # use the URL to the github repo
     url='https://github.com/luisandresgc/fedexrates',
-    download_url='https://github.com/luisandresgc/fedexrates/tarball/0.1',
+    download_url='https://github.com/luisandresgc/fedexrates/tarball/0.3',
     keywords=['testing', 'logging', 'example'],
     classifiers=[ ],
     license='MIT',
